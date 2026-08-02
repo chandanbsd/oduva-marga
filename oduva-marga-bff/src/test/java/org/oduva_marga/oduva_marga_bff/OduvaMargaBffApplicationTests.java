34324f7d645c805647d24896ce4bc99de81d4b41
channaveer swamy <channaveer.b.swamy@gmail.com>
2026-08-02 08:48:17 +0530

    bff initial commit

---
+++ oduva-marga-bff/src/test/java/org/oduva_marga/oduva_marga_bff/OduvaMargaBffApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oduva_marga.oduva_marga_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OduvaMargaBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
