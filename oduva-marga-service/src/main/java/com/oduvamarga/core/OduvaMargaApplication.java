97482099015338a6c0e3669fa56c63cbc0dcabe8
channu <channaveer.b.swamy@gmail.com>
2026-09-04 08:07:22 +0530

    update code to use virtual thread

--- oduva-marga-service/src/main/java/com/oduvamarga/core/OduvaMargaApplication.java
+++ oduva-marga-service/src/main/java/com/oduvamarga/core/OduvaMargaApplication.java
@@ -4,15 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
 public class OduvaMargaApplication {
 
 	@GetMapping("/")
-	public Mono<String> home() {
-		return Mono.just("hello");
+	public String home() {
+		return "hello";
 	}
 
 	static void main(String[] args) {
--- oduva-marga-service/src/main/java/com/oduvamarga/core/configuration/SecurityFilterConfiguration.java
+++ oduva-marga-service/src/main/java/com/oduvamarga/core/configuration/SecurityFilterConfiguration.java
@@ -4,31 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityFilterConfiguration {
 
     @Bean
-    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
         http.oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor()))
                 )
-                .authorizeExchange((authorize) -> authorize
-                        .anyExchange().authenticated()
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
     }
 
-    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+    Converter<Jwt, AbstractAuthenticationToken> grantedAuthoritiesExtractor() {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JwtRoleConverter());
-        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+        return jwtAuthenticationConverter;
     }
 }
