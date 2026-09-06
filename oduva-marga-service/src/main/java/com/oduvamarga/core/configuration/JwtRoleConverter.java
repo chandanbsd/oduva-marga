18389d7ad33883fc5fe3dd162b6a6a8802f1d6f0
channavee-r <68196045+channavee-r@users.noreply.github.com>
2026-09-06 12:34:45 +0530

    setup db schema and add lombok (#89)
    * update code to use virtual thread
    
    * add lombok & add db_schema name to properties

--- oduva-marga-service/src/main/java/com/oduvamarga/core/configuration/JwtRoleConverter.java
+++ oduva-marga-service/src/main/java/com/oduvamarga/core/configuration/JwtRoleConverter.java
@@ -1,5 +1,6 @@
 package com.oduvamarga.core.configuration;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class JwtRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     @Override
-    public Collection<GrantedAuthority> convert(Jwt jwt) {
+    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {
         Map<String, List<String>> realmAccess = jwt.getClaim("realm_access");
         assert realmAccess != null;
         List<String> roles = realmAccess.getOrDefault("roles", Collections.emptyList());
